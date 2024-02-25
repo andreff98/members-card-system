547aa362e6408eec3041492965c77147e6336cab
Andre Ferreira <andreferreira6@hotmail.com>
2024-02-25 16:12:38 +0000

    Jwt generator added AuthorizationExample

--- backend/EvceApi/Controllers/AuthenticationController.cs
+++ backend/EvceApi/Controllers/AuthenticationController.cs
@@ -1,11 +1,15 @@
 ﻿using EvceApi.Dtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace EvceApi.Controllers;
 
 [Route("api/v1/[controller]")]
 [ApiController]
-public class AuthenticationController : ControllerBase
+public class AuthenticationController(IConfiguration configuration) : ControllerBase
 {
     [HttpPost("register")]
     public IActionResult Register()
@@ -16,6 +20,26 @@ public class AuthenticationController : ControllerBase
     [HttpPost("login")]
     public IActionResult Login([FromBody] LoginDto loginDto)
     {
-        return Ok();
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                new Claim("user", loginDto.Username),
+                new Claim("isAdmin", "true"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            }),
+            Expires = DateTime.UtcNow.AddHours(2),
+            Issuer = configuration["Jwt:Issuer"],
+            Audience = configuration["Jwt:Audience"],
+            SigningCredentials = credentials
+        };
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        var jwtToken = tokenHandler.WriteToken(token);
+
+        return Ok(jwtToken);
     }
 }
--- backend/EvceApi/Controllers/CardsController.cs
+++ backend/EvceApi/Controllers/CardsController.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace EvceApi.Controllers;
 
@@ -6,5 +8,21 @@ namespace EvceApi.Controllers;
 [ApiController]
 public class CardsController : ControllerBase
 {
-   
+    [Authorize]
+    [HttpGet]
+   public IActionResult GetCards()
+    {
+
+        // Access username claim from user's identity
+        var usernameClaim = User.Claims.FirstOrDefault(c => c.Type == "user");
+        var isAdminClaim = User.Claims.FirstOrDefault(c => c.Type == "isAdmin");
+        var username = usernameClaim?.Value;
+        var isAdmin = isAdminClaim?.Value;
+
+        return new JsonResult(new { 
+            Cards = new[] { "card1", "card2", "card3" }, 
+            User = username,
+            Admin = isAdmin
+        });
+    }
 }
